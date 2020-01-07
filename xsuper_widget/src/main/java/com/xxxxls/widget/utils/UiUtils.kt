2d69227918816e42d266a1efc5af9953513d67f0
max <mxruan.office@gmail.com>
2020-01-07 16:26:47 +0800

    add 新加widget模块

--- xsuper_widget/src/main/java/com/xxxxls/widget/utils/UiUtils.kt
+++ xsuper_widget/src/main/java/com/xxxxls/widget/utils/UiUtils.kt
@@ -1,4 +1,4 @@
-package com.xxxxls.utils
+package com.xxxxls.widget.utils
 
 import android.app.Activity
 import android.content.Context
@@ -8,39 +8,10 @@ import android.util.TypedValue
 /**
  *
  * @author Max
- * @date 2019-12-10.
+ * @date 2020-01-03.
  */
 
-fun Long.dp(): Float {
-    return UiUtils.px2dip(this.toFloat())
-}
-
-
-fun Float.dp(): Float {
-    return UiUtils.px2dip(this)
-}
-
-
-fun Int.dp(): Float {
-    return UiUtils.px2dip(this.toFloat())
-}
-
-
-fun Long.px(): Float {
-    return UiUtils.dip2px(this.toFloat())
-}
-
-
-fun Float.px(): Float {
-    return UiUtils.dip2px(this)
-}
-
-
-fun Int.px(): Float {
-    return UiUtils.dip2px(this.toFloat())
-}
-
-object UiUtils {
+internal object UiUtils {
     fun getScreenWidth(context: Context): Int {
         val localDisplayMetrics = DisplayMetrics()
         (context as Activity).windowManager.defaultDisplay.getMetrics(localDisplayMetrics)
@@ -57,20 +28,6 @@ object UiUtils {
         return getScreenWidth(context) * 1.0f / getScreenHeight(
             context
         )
-    }
-
-    /**
-     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
-     */
-    fun dip2px(dpValue: Float): Float {
-        return dip2px(AppUtils.getApp(), dpValue)
-    }
-
-    /**
-     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
-     */
-    fun px2dip(pxValue: Float): Float {
-        return px2dip(AppUtils.getApp(), pxValue)
     }
 
     /**
