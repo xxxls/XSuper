7d9d35fd0cb5eaf4c9fe32251c1e5cec154e988d
Max <mxruan@163.com>
2021-01-17 21:10:29 +0800

    style：分页列表数据展示微调

--- androidXSuper/app/src/main/java/com/xxxxls/example/ui/paging/adapter/ArticleItemViewHolder.kt
+++ androidXSuper/app/src/main/java/com/xxxxls/example/ui/paging/adapter/ArticleItemViewHolder.kt
@@ -1,16 +1,18 @@
 package com.xxxxls.example.ui.paging.adapter
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.xxxxls.example.R
 import com.xxxxls.example.ui.paging.data.bean.PagingItemBean
 
 /**
  * @author Max
  * @date 12/23/20.
  */
-class ArticleItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class ArticleItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
     fun bindData(item: PagingItemBean, position: Int) {
-
+        view.findViewById<TextView>(R.id.tv_title)?.text = item.title ?: ""
     }
 }
